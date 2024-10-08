91d8678880a95a833fecebf6a6ca9f6851598d87
G00419446@atu.ie <jseeruthen26@outlook.coom>
2024-10-08 10:40:08 +0100

    Added code so the user can add product from local host

--- src/main/java/ie/atu/productapp/ProductController.java
+++ src/main/java/ie/atu/productapp/ProductController.java
@@ -1,8 +1,7 @@
 package ie.atu.productapp;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,5 +18,12 @@ public class ProductController {
     public List<Product> getProduct()
     {
         return productList;
+    }
+
+    @PostMapping("addProduct")
+    public ResponseEntity<List> addProduct(@RequestBody Product product)
+    {
+        productList.add(product);
+        return ResponseEntity.ok(productList);
     }
 }
